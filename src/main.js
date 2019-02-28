8630bd734e09ca7481973a70b9eab0db60dc6172
xuxue <xuxue@17shihui.com>
2019-02-28 13:49:28 +0800

    去除mian引用

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App';
 import router from './router';
 import {store} from './store/store';
 import {getCookies} from "./utils/cookie";
+import {deCry} from "./utils/encrypt";
 
 import 'element-ui/lib/theme-chalk/index.css';    // 默认主题
 
 import "babel-polyfill";
 import {validateToken} from './utils/token'
 import {TOKEN,LOCALKEY} from "./constants/constants";
-import CryptoJS from 'crypto-js'
 import {ADMINPORTAL} from "./api/api";
 
 import './assets/loginImage/back.png'
@@ -21,8 +21,7 @@ const redirecturl = search?search.match(/redirecturl=(\S*)&type/)[1]:null
 
 if(redirecturl){
     if(value){
-        const tokenStr = CryptoJS.AES.decrypt(value,LOCALKEY)
-        const token = tokenStr.toString(CryptoJS.enc.Utf8);
+        const token = deCry(value,LOCALKEY)
         validateToken(token).then(
             () => {
                 new Vue({
@@ -44,7 +43,3 @@ if(redirecturl){
 }else{
     window.location.href=ADMINPORTAL
 }
-
-
-
-
